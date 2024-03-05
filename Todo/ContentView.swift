391157e60ab8f630da8f6adae42daede82149770
liliang <1497809786@qq.com>
2024-03-05 21:36:55 +0800

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by znz on 2024/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by znz on 2024/3/5.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
